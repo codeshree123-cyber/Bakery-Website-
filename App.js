7f38a9fefbe60840b9b89b18fe0bdd72d6b6e4da
codeshree123-cyber <codeshree123@gmail.com>
2025-11-16 18:56:53 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,29 @@
+import Header from "./components/Header";
+import Home from "./components/Home";
+import About from "./components/About";
+import Menu from "./components/Menu";
+import Products from "./components/Products";
+import Review from "./components/Review";
+import Contact from "./components/Contact";
+import Blogs from "./components/Blogs";
+import Footer from "./components/Footer";
+
+import "./assets/style.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+      <About />
+      <Menu />
+      <Products />
+      <Review />
+      <Contact />
+      <Blogs />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
